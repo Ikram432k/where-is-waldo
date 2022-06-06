666c970c9cc8fb177aed307c67920a6bfa265ac1
ikram mohideen <ikram432k@gmail.com>
2022-06-06 22:22:03 +0530

    src/components/header/header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import{ Div } from './headerStyle'
-function Head(){
+
+interface propstime{
+    sec:number;
+    min:number;
+}
+
+function Head({sec,min}:propstime){
 return(
     <Div>
         <h1>where is wally ?</h1>
-        <p>00:00:00</p>
+        <p>{String(min).padStart(2,'0')}:{String(sec).padStart(2,'0')}</p>
     </Div>
 )
 }
