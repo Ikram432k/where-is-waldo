7a23af3b7088960df4d261baa1ed251132c8c98c
ikram mohideen <ikram432k@gmail.com>
2022-06-07 13:28:33 +0530

    src/components/getName/getName.tsx

--- src/components/getName/getName.tsx
+++ src/components/getName/getName.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
+import { addPlayers } from '../backend/backend';
+import { Div,Form } from './getNameStyle';
+import ScoreBoard from '../scoreBoard/scoreBoard';
 
 interface getnameprops{
     sec:number;
     min:number;
+    askName:boolean;
 }
-const GetName =({sec,min}:getnameprops)=>{
+const GetName =({askName,sec,min}:getnameprops)=>{
 
     const [name,setName] = useState<string>('');
+    const [hide,setHide] = useState<boolean>(false)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void =>{
         setName(e.target.value);
@@ -15,21 +20,26 @@ const GetName =({sec,min}:getnameprops)=>{
         e.preventDefault();
         const totalTime = min * 60 + sec;
         if(name){
-            
+            // addPlayers(name,min,sec,totalTime);
+            setHide(true);
+            console.log(totalTime);
         }
     }
     return(
-        <div>
-        <p>you finished the game in  {String(min).padStart(2, '0')}:{String(sec).padStart(2, '0')}</p>
-        <form onSubmit={handleSubmit}>
+        <Div visible={askName}>
+        <Form onSubmit={handleSubmit} hide={hide}>
+            <p>Congratulations !</p>
+            <p>you finished the game in  {String(min).padStart(2, '0')}:{String(sec).padStart(2, '0')}</p>
+            <h3>enter your name and check the leaderboard</h3>
             <input
             onChange={handleChange}
             type="text"
             placeholder="Enter your name"
             />
             <button>Enter</button>
-        </form>
-        </div>
+        </Form>
+        <ScoreBoard hide={hide}/>
+        </Div>
     )
 }
 export default GetName;
