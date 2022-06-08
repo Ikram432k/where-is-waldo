4ac36e54819982606a84fcc2b3f7b956b75abe55
ikram mohideen <ikram432k@gmail.com>
2022-06-08 11:14:26 +0530

    src/components/header/header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import{ Div } from './headerStyle'
+import { Button } from "../startPage/startPageStyle";
 
 interface propstime{
     sec:number;
     min:number;
+    restart:any;
 }
 
-function Head({sec,min}:propstime){
+function Head({sec,min,restart}:propstime){
 return(
     <Div>
         <h1>where is wally ?</h1>
         <p>{String(min).padStart(2,'0')}:{String(sec).padStart(2,'0')}</p>
+        <Button onClick={restart}>restart</Button>
     </Div>
 )
 }
